274809ef8e08aa3e0d4bd998845a5e6848ccd0fc
George Lewin-Smith <gls1@hotmail.co.uk>
2018-11-12 18:48:53 +0000

    finsihed the work which now works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(email: "k8hunter@hotmail.com", password: "difficult_password")
+user.save!

c49b085d3aab98e1fc148de9458d5ca335c53686
George Lewin-Smith <gls1@hotmail.co.uk>
2018-11-13 16:41:09 +0000

    cleaning booking branch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+before_action :find, only: %w[show edit update destroy]
+
+  def index
+    @booking = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    raise
+    # @booking = Booking.last
+    @booking = Booking.new(set_params)
+    @booking.user = current_user
+
+    if @device.save
+      redirect_to devices_path
+    else
+      render :new
+    end
+  end
+
 end
--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -7,6 +7,7 @@ class DevicesController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new

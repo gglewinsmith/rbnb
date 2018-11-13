9b9aeea8e3044febdf04a041f5151f8b1d7d6d83
Angus Watson <angus.watson16@gmail.com>
2018-11-13 14:23:05 +0000

    updated show page and other bits

--- app/models/device.rb
+++ app/models/device.rb
@@ -5,7 +5,7 @@ class Device < ApplicationRecord
 
   validates :name, presence: true
   validates :price_per_week, presence: true
-  validates :text, presence: true
+  validates :description, presence: true
   validates :condition_of_device, presence: true
   validates :age_of_device, presence: true
   validates :type_of_device, presence: true

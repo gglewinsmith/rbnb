7201017485defe2d932add2258c5b02ec988b9aa
Angus Watson <angus.watson16@gmail.com>
2018-11-14 16:43:16 +0000

    added map with markers and windows

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,6 +3,15 @@ class DevicesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    @devices = Device.where.not(latitude: nil, longitude: nil)
+
+    @markers = @devices.map do |device|
+      {
+        lng: device.longitude,
+        lat: device.latitude,
+        infoWindow: { content: render_to_string(partial: "/shared/map_window", locals: { device: device }) }
+      }
+    end
     if current_user
       @devices = Device.all.reject { |device| device.user == current_user }
     else
@@ -47,7 +56,7 @@ class DevicesController < ApplicationController
   private
 
   def set_params
-    params.require(:device).permit(:name, :price_per_week, :description, :condition_of_device, :age_of_device, :type_of_device, :photo)
+    params.require(:device).permit(:name, :price_per_week, :description, :condition_of_device, :age_of_device, :type_of_device, :address, :photo)
   end
 
   def set_device

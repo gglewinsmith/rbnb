2b33e6c093482b3e973d626f52cf19879a81b4a9
Angus Watson <angus.watson16@gmail.com>
2018-11-14 10:19:36 +0000

    completed booking for dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,11 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    bookings = Booking.all
-    devices_booked = []
+    @bookings = Booking.all
+    @devices_booked = []
     current_user_id = current_user.id
-    bookings.each do |booking|
-      devices_booked << booking if booking.user_id == current_user_id
+    @bookings.each do |booking|
+      @devices_booked << booking if booking.user_id == current_user_id
     end
   end
 end

03c85b6c34ece8b34665e23440ebd5769d3e99ae
Angus Watson <angus.watson16@gmail.com>
2018-11-14 11:44:01 +0000

    dashboard now shows borrowing and lending for user - not just borrowed devices as lent devices

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,8 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @booked_devices = Booking.all.select { |booking| booking.user == current_user}
+    @lent_devices = Booking.all.select { |booking| booking.device.user == current_user}
     @bookings = Booking.all
     @devices_booked = []
     current_user_id = current_user.id

0d26e944b5c817a3850a20007b136eeb3d85da63
George Lewin-Smith <gls1@hotmail.co.uk>
2018-11-15 14:42:37 +0000

    added avatar picture to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   before_action :authenticate_user!
 
   private
@@ -11,4 +13,10 @@ class ApplicationController < ActionController::Base
   def store_user_location!
     store_location_for(:user, request.fullpath)
   end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :photo_cache])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-
+  mount_uploader :photo, PhotoUploader
   has_many :devices
   has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
+  validates :photo, presence: true
 end

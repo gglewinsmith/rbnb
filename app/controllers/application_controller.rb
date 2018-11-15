678d177c08ca9feda1b0ddbb881bc7e39b6b2b38
Kate Buxton <k8hunter@hotmail.com>
2018-11-15 11:41:19 +0000

    user journey improved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
+  before_action :authenticate_user!
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
